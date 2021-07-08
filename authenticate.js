0792ea4b506d85b7f7e72c381195d317714dc83a
Jonathan Cardenas <jonscardenas@gmail.com>
2021-07-08 13:12:53 -0500

    week 4 oauth exercise

--- authenticate.js
+++ authenticate.js
@@ -9,6 +9,11 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
 
+//for fb oauth
+const FacebookTokenStrategy = require('passport-facebook-token');
+
+
+
 const config = require('./config.js')
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
@@ -54,3 +59,35 @@ exports.verifyAdmin = function(req, res, next){
         return next(err);
     }
 };
+
+//checks if user exists in db and returns it if so. If user doesn't exist, a new one is created based on facebook profile information
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({facebookId: profile.id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+    )
+);
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '2900752003532134',
+        clientSecret: '045eb235418b37b77a3fc4c048c73d00'
+    }
 }
